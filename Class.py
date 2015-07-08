5386655a26e9391a58b1bc763f547757cf7d0891
vendidero <dennis.nissle@iwebix.de>
2015-07-08 10:26:26 +0200

    train

--- Class.py
+++ Class.py
@@ -5,8 +5,19 @@ class Class():
 	def __init__(self, name, content, count):
 		self.name = name
 		self.contentRaw = content
-		self.tokens = Tokenizer(content);
+		self.tokens = Tokenizer(content)
+		self.condProb = self.tokens.getTokens()
 		self.count = count
+		self.prior = 0.0
+
+	def setPrior(self, prior):
+		self.prior = prior
+
+	def condProbs(self):
+		return self.condProb
+
+	def condProb(self, token):
+		return self.condProb[token]
 
 	def getName(self):
 		return self.name
--- ClassBank.py
+++ ClassBank.py
@@ -5,11 +5,13 @@ class ClassBank():
 	
 	def __init__(self):
 		self.classes = {}
+		self.documentCount = 0
 		self.tokenizer = Tokenizer("");
 
 	def addClass(self, classInst):
 		self.classes[ classInst.getName() ] = classInst
 		self.tokenizer.tokenize(classInst.contentRaw)
+		self.documentCount = self.documentCount + classInst.count
 
 	def getClass(self, name):
 		if name in self.classes:
@@ -24,4 +26,15 @@ class ClassBank():
 
 	def getVocabularySum(self):
 		return len(self.tokenizer.getTokens())
+
+	def train(self):
+		v = self.getVocabulary().getTokens()
+		n = self.documentCount
+		for c in self.classes:
+			c = self.classes[c]
+			c.setPrior(c.count/n)
+			t = c.getTokens().getTokens()
+			for key in c.condProb:
+				c.condProb[key] = (float) (t[key] + 1) / (len(t) + v[key])
+
 
